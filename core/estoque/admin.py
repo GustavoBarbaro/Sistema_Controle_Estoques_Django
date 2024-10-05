3d14916154afb60489854d74f5f50ea97519279d
Gustavo <gustavobarbarooliveira@hotmail.com>
2024-10-05 14:45:01 -0300

    Primeira versao do banco

--- core/estoque/admin.py
+++ core/estoque/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
+
 
 # Register your models here.
+
+admin.site.register(Produto)
+admin.site.register(Categoria)
+admin.site.register(Fornecedor)
+admin.site.register(Usuario)
+admin.site.register(Movimentacao)
--- core/estoque/models.py
+++ core/estoque/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
 
@@ -32,15 +34,11 @@ class Produto(models.Model):
         return self.nome
 
 
-""" class Usuario(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    #senha = models.CharField(max_length=255)
-    funcao = models.CharField(max_length=255)
-    tag_rfid = models.CharField(max_length=255, unique=True)
+class Usuario (AbstractUser):
+    tag_rfid = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return self.nome """
+        return self.username
 
 
 class Movimentacao(models.Model):
